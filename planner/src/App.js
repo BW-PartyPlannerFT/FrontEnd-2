4f7472aab5eb9d951691d0173b115924c6f93082
jordan <jordan@jordans-Air.lan>
2019-11-18 19:02:34 +0700

    added routing for login and signup

--- planner/src/App.js
+++ planner/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-    
+    <Route exact path="/" components={Login} />
+    <Route path="/signup" components={Signup}/>
     </div>
   );
 }
--- planner/src/components/Login.js
+++ planner/src/components/Login.js
@@ -1,6 +1,8 @@
 // Login form with input for username and password. 
 // State handled locally.
 // On submit redirects to private route (PartyList)
+import React from 'react';
+
 function Login() {
     return (
         <div className="Login">
