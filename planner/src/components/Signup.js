c7664818232eccf504a7dbdf22a523dd05fd268d
jordan <jordan@jordans-Air.lan>
2019-11-18 20:00:38 +0700

    started signup page

--- planner/src/components/Signup.js
+++ planner/src/components/Signup.js
@@ -8,6 +8,19 @@ import { Link } from 'react-router-dom';
 export default function signup(){
     return(
         <div className="Signup-Links">
+            <h1>REGISTER</h1>
+            <label>
+                   USERNAME
+                   <input type='text' />
+               </label>
+               <label>
+                   EMAIL
+                   <input type='text'/>
+               </label>
+               <label>
+                   Password:
+                   <input type='password' />
+               </label>
             <Link to="/">
 
             </Link>
