39b9f0beebcae67c838a1ae6ded55393eac676aa
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2019-11-19 15:00:42 -0800

    Date Picker Added

--- planner/src/components/PartyForm.js
+++ planner/src/components/PartyForm.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import DatePicker from "./DatePicker";
+import { Form, Input } from 'reactstrap';
+ 
 
 const PartyForm = props => {
   console.log("props", props);
@@ -6,14 +9,6 @@ const PartyForm = props => {
 
   const handleChanges = e => {
     console.log(note);
-    // console.log("the name", e.target.name)
-    // console.log("the event target", e.target)
-    //we are dynamically setting our keys.
-    // setNote({ ...note, title: e.target.value });
-    // setNote({ ...note, note: e.target.value });
-    //  const nameObj={...note}
-    //  nameObj[e.target.name]= e.target.value
-    // setNote(nameObj)
     setNote({ ...note, [e.target.name]: e.target.value });
   };
 
@@ -25,54 +20,51 @@ const PartyForm = props => {
 
   return (
     
+    <Form>
+      
     <form onSubmit={submitForm}>
-      <div><label htmlFor="host">Host</label></div>
-      <div><input
+      {/* <div><label htmlFor="host">Host:</label></div>
+      <div><Input
         id="text"
         type="text"
         name="host"
         onChange={handleChanges}
         value={note.host}
-      /></div>
-      <div><label htmlFor="name">Party Title</label></div><div>
+      /></div> */}
+      
+      <DatePicker />
+      
+      {/* <div><label htmlFor="name">Party Title:</label></div><div>
       <textarea
         id="name"
         name="name"
         onChange={handleChanges}
         value={note.name}
       /></div>   
-      <div><label htmlFor="theme">Theme</label></div><div>
+      <div><label htmlFor="theme">Theme:</label></div><div>
       <textarea
         id="theme"
         name="theme"
         onChange={handleChanges}
         value={note.theme}
       /></div>
-            <div><label htmlFor="guest">Number of expected Guest</label></div>
-      <div><input
+            <div><label htmlFor="guest">Number of expected guests:</label></div>
+      <div><Input
         id="guest"
         type="text"
         name="guest"
         onChange={handleChanges}
         value={note.guest}
       /></div>
-            <div><label htmlFor="description">Descripition</label></div>
-      <div><input
-        id="descrption"
-        type="text"
-        name="description"
-        onChange={handleChanges}
-        value={note.description}
-      /></div>
-            <div><label htmlFor="budget">Budget</label></div>
-      <div><input
+            <div><label htmlFor="budget">Budget:</label></div>
+      <div><Input
         id="budget"
         type="text"
         name="budget"
         onChange={handleChanges}
         value={note.budget}
-      /></div>
-      <div>Category</div>
+      /></div> */}
+      {/* <div>Category:</div>
       <select class="form-control form-control-lg">
         <option>Category</option>
         <option>Adult Birthday Party</option>
@@ -81,10 +73,10 @@ const PartyForm = props => {
         <option>Kid's Birthday Party</option>
         <option>Charity Party</option>
         </select>
-        <label></label>
+        <label></label> */}
 
-      <div><button type="submit">Let's Party</button></div>
-    </form>
+      <div><button type="submit">Let's Party!</button></div>
+    </form></Form>
   );
 };
 
