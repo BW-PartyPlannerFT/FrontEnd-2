09b627ef7bab67f1b5e6e0ad28e33e13d36d4cf7
Chrismis79 <chrismis79@gmail.com>
2019-11-20 08:03:47 -0600

    redux set up done for category

--- planner/src/redux/rootReducer.js
+++ planner/src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import partyReducer from './parties/reducer';
 import picturesReducer from './pictures/reducers';
+import categoryReducer from './categories/reducer';
 
 export default combineReducers({
     partyReducer,
-    picturesReducer
+    picturesReducer,
+    categoryReducer
 })
