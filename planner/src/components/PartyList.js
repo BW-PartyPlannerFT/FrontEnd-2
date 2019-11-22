aa3f2b12e78ebf910b6921cce290f7c838ef65ad
jordan <jordan@jordans-Air.lan>
2019-11-22 19:47:58 +0700

    update party list

--- planner/src/components/PartyList.js
+++ planner/src/components/PartyList.js
@@ -1,9 +1,3 @@
-// Displays a grid of parties that are in process of being planned. 
-// Ability to delete party.
-// This component will render PartyBoard
-// Click on party and navigates to PartyBoard
-// Button to create new party that redirects you to PartyForm
-
 import React, { useEffect } from 'react';
 // import AppNav from '../components/AppNav';
 import { Link } from 'react-router-dom';
@@ -55,45 +49,6 @@ if(props.isLoading){
   return <h2><span role="img" aria-labelledby='jsx-ally/accessible-emoji'>🔃</span></h2>
 }else{
   
-<<<<<<< HEAD
-  if(props.isLoading){
-    return <h2><span role="img" aria-labelledby='jsx-ally/accessible-emoji'>🔃</span></h2>
-}
-return (
-  <>
-    <div>
-        {/* {props.error && <p>{props.error}</p>} */}
-        {/* {props.parties.map(item => { */}
-          
-            {/* return ( */}
-                {/* <PartyBoard key={item.id} 
-                            party_name={item.party_name} 
-                            host={item.host} 
-                            guests={item.guests}
-                            theme={item.theme}
-                            date={item.date}
-                            budget={item.budget}/>
-            )
-        } */}
-       </div>
-       <div>
-         <h1 className="party-h1">Select a Party to Plan</h1>
-        <Link to="/partyform">
-          <button className="add-list">ADD PARTY</button>
-        </Link>
-      </div>
-      </>
-    );
-};
-//data will go here
-
-const mapStateToProps = state => {
-  return {
-    parties: state.parties,
-    error: state.error,
-    isLoading: state.isLoading
-  }
-=======
   return (
 
     <>
@@ -137,7 +92,6 @@ const mapStateToProps = state => {
     </>
   )
 }
->>>>>>> d6519b226c6190cad3e30475bad0d728046230e7
 }
 
 
@@ -153,6 +107,5 @@ const mapStateToProps = state => {
   }
 
 export default connect(mapStateToProps, {rxGetParty})(PartyList); 
-
   
   
