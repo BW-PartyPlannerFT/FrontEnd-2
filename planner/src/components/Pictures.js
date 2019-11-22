122cecbfb83d3aa4276e9cc0d892cb9905f88eda
Chrismis79 <chrismis79@gmail.com>
2019-11-21 20:42:45 -0600

    working on pic upload

--- planner/src/components/Pictures.js
+++ planner/src/components/Pictures.js
@@ -3,15 +3,31 @@
 // Input to add image url and upload pics for saved ideas.
 // Ability to delete  picture
 //Rendered by PartyBoard
-import React from 'react';
+// import React from 'react';
+// import ImageUpload from './ImageUpload';
+// import Pic from './Pic';
 
 
-const Pictures = (props) => {
-    return (
-      <div>
-          <img src={props.url} alt = 'party ideas'/>
-          <p>{props.url}</p>
-      </div>
-    );
-};
-export default Pictures;
+// const Pictures = (props) => {
+//   console.log('pictures props', props)
+  
+//   const addImage = pics => {
+//     setInput([...input, pics])
+//   }
+//     return (
+//       <div>
+//           <ImageUpload addImage={addImage}/>
+//           <img src={props.url} alt = 'party ideas'/>
+//           {props.members.map(pic => {
+//             return (
+//             <Pic key={pic.url}
+//                      input={pic.input}
+//                       setInput={setInput}/>
+                               
+//           )})}
+          
+//           <p>{props.url}</p>
+//       </div>
+//     );
+// };
+// export default Pictures;
